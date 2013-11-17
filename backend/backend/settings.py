a07e69907b0e384c4d6808276ea6691f0640f554
Jingkai He <jaxihe@gmail.com>
2013-11-17 01:44:12 +0000

    rest api

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'foodchain',
+    'rest_framework'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -48,6 +49,22 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
+REST_FRAMEWORK = {
+    # Use hyperlinked styles by default.
+    # Only used if the `serializer_class` attribute is not set on a view.
+    'DEFAULT_MODEL_SERIALIZER_CLASS':
+        'rest_framework.serializers.HyperlinkedModelSerializer',
+
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
+
+    'DEFAULT_RENDER_CLASSES': (
+        'reset_framework.renderers.JSONRenderer'
+        )
+}
 ROOT_URLCONF = 'backend.urls'
 
 WSGI_APPLICATION = 'backend.wsgi.application'
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,12 +1,24 @@
 from django.conf.urls import patterns, include, url
 
+from foodchain.models import Kitchen, Recipient
+from rest_framework import viewsets, routers
+
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'backend.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
+class KitchenViewSet(viewsets.ModelViewSet):
+    model = Kitchen
+
+class RecipientViewSet(viewsets.ModelViewSet):
+    model = Recipient
+
+router = routers.DefaultRouter()
+router.register(r'kitchens', KitchenViewSet)
+router.register(r'recipients', RecipientViewSet)
+
+urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
