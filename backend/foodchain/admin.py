d5766be939e25d168b2fa5d263042deba327f9ef
Marijonas <marpetr@gmail.com>
2013-11-17 00:24:38 +0000

    Recipient import

--- backend/foodchain/admin.py
+++ backend/foodchain/admin.py
@@ -4,7 +4,7 @@ from django.conf.urls import url, patterns
 from django.shortcuts import render, redirect
 from django import forms
 
-from foodchain.models import Kitchen, Recipient
+from foodchain.models import Kitchen, Recipient, Meal
 from foodchain import csv_import
 
 # Register your models here.
@@ -33,7 +33,36 @@ class KitchenAdmin(admin.ModelAdmin):
                 return redirect('admin:foodchain_kitchen_changelist')
         else:
             form = CSVForm()
-        return render(request, 'admin/foodchain/kitchen/import_csv.html', { 'form': form })
+        return render(request, 'admin/foodchain/import_csv.html', { 'form': form })
 
 admin.site.register(Kitchen, KitchenAdmin)
-admin.site.register(Recipient)
+
+
+class MealInline(admin.TabularInline):
+    model = Meal
+    extra = 0
+
+class RecipientAdmin(admin.ModelAdmin):
+    list_display = ['booking_id', 'nickname', 'postcode']
+    inlines = [MealInline]
+    
+    def get_urls(self):
+        urls = super(RecipientAdmin, self).get_urls()
+        my_urls = patterns('',
+            url(r'^import_csv/$', self.admin_site.admin_view(self.import_csv), name='recipient-import-csv')
+        )
+        return my_urls + urls
+    
+    def import_csv(self, request):
+        if request.method == 'POST':
+            form = CSVForm(request.POST, request.FILES)
+            if form.is_valid():
+                csv_contents = form.cleaned_data['file'].read()
+                csv_import.import_recipients(csv_contents)
+                self.message_user(request, 'Import succeeded')
+                return redirect('admin:foodchain_recipient_changelist')
+        else:
+            form = CSVForm()
+        return render(request, 'admin/foodchain/import_csv.html', { 'form': form })
+
+admin.site.register(Recipient, RecipientAdmin)
--- backend/foodchain/csv_import.py
+++ backend/foodchain/csv_import.py
@@ -1,4 +1,5 @@
 import csv
+import itertools
 
 from foodchain import models
 
@@ -16,3 +17,20 @@ def import_kitchens(csv_file):
         k.postcode = item['Postcode']
         k.capacity = int(item['Capacity'])
         k.save()
+
+def import_recipients(csv_file):
+    reader = csv.DictReader(csv_file.splitlines())
+    items = list(reader)
+    models.Recipient.objects.all().delete()
+    print items
+    for k, g in itertools.groupby(items, lambda item: item['Booking ID']):
+        group = list(g)
+        if not k:
+            raise Exception('Booking ID contains empty value')
+        recipient = models.Recipient()
+        recipient.nickname = group[0]['Nickname'].decode('latin1')
+        recipient.postcode = group[0]['Primary Postal Code']
+        recipient.booking_id = k
+        recipient.save()
+        for meal in group:
+            recipient.meal_set.create(meal_type=meal['Meal Type'], comment=meal['Comments'])
