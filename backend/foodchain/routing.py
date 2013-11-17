873d31295f4bcbaf2af7cd22fc0bc508ba621b4b
QSilver <geo_007.popa@yahoo.com>
2013-11-17 03:36:10 +0000

    Smart scheduler

--- backend/foodchain/routing.py
+++ backend/foodchain/routing.py
@@ -1,3 +1,5 @@
+import math
+
 from foodchain import models
 
 def handle_scheduling(form_data):
@@ -6,6 +8,8 @@ def handle_scheduling(form_data):
     algorithm = form_data.pop('algorithm')
     if algorithm == 'dummy':
         return dummy_scheduler(**form_data)
+    elif algorithm == 'smart':
+        return smart_scheduler(**form_data)
     raise Exception('Algorithm not implemented')
 
 def dummy_scheduler(**params):
@@ -26,3 +30,41 @@ def dummy_scheduler(**params):
         
         drive.meals_to_deliver = sum(d.recipient.meal_set.count() for d in drive.delivery_set.all())
         drive.save()
+
+def dist(a, b):
+    return math.hypot(a.lat - b.lat, a.lng - b.lng)
+
+def smart_scheduler(max_journey_duration, max_meals_per_drive):
+    kitchens = models.Kitchen.objects.all()
+    recipients = models.Recipient.objects.all()
+    for r in recipients:
+        r.num_meals = r.meal_set.count()
+
+    for k in kitchens:
+        k.pool = []
+
+    for r in recipients:
+        r.kitchen = min(kitchens, key=lambda k: dist(r, k))
+        r.kitchen.pool.append(r)
+
+    for k in kitchens:
+        print 'Kitchen', k.lat, k.lng
+        r_set = set(k.pool)
+        while r_set:
+            first = min(r_set, key=lambda r: dist(k, r))
+            r_set.remove(first)
+            route = [first]
+            meals_left = max_meals_per_drive - first.num_meals
+
+            while r_set:
+                prev = route[-1]
+                next_node = min(r_set, key=lambda r: dist(prev, r))
+                if meals_left - next_node.num_meals < 0:
+                    break
+                route.append(next_node)
+                meals_left -= next_node.num_meals
+                r_set.remove(next_node)
+
+            print 'Route', ', '.join('%s;%s' % (r.lat, r.lng) for r in route)
+
+
