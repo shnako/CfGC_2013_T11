354aa7480f98f82f2288ea34a45ea62657c604dc
Marijonas <marpetr@gmail.com>
2013-11-17 08:42:23 +0000

    get_deliveries API

--- backend/foodchain/urls.py
+++ backend/foodchain/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^api/user_check$', 'foodchain.api.user_check'),
-    url(r'^api/is_delivery_assigned$', 'foodchain.api.is_delivery_assigned')
+    url(r'^api/is_delivery_assigned$', 'foodchain.api.is_delivery_assigned'),
+    url(r'^api/get_deliveries$', 'foodchain.api.get_deliveries')
 )
